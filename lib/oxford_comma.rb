6f12602f879cc473569b9919d5af46cfcbf0b488
Dario Carlino <baires82@gmail.com>
2020-08-21 01:30:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    new_array = array[0..1].join(", ")
+    new_array << ", and #{array[2]}"
+  else
+    last_element = array[-1]
+    array.pop
+    array << "and #{last_element}"
+    array.join(", ")
 
+  end
 end
