8a6f1f12c2c33c9e27a66c99db5f931718fc8b4a
Brenda Ferrufino <ferrufinob@gmail.com>
2020-08-22 04:43:43 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,18 +7,3 @@
   return array.join(', ')
 end
 end
-
-
-# def oxford_comma(array)
-# case
-# array.size 
-#   when 0
-#   "" 
-#   when 1
-#     array.first
-#   when 2
-#     array.join(" and ")
-#   else
-#     [array[0..-2].join(", "), array.last].join(", and ")
-#   end
-# end
