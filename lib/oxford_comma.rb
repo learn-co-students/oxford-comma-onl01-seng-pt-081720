61fbfd1a3c9b0b6ded91de109e9395cafec8d2a6
Brenda Ferrufino <ferrufinob@gmail.com>
2020-08-22 00:59:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,24 @@
-def oxford_comma(array)
- case
- array.size 
-  when 0
-   "" 
-  when 1
-    array.first
-  when 2
-    array.join(" and ")
-  else
-    [array[0..-2].join(", "), array.last].join(", and ")
-  end
+
+ def oxford_comma(array)
+   if array.size < 3
+  return array.join(' and ')
+  else array.size > 3
+  array[-1] = "and " + array[-1]
+  array.join(', ')
 end
+end
+
+
+# def oxford_comma(array)
+# case
+# array.size 
+#   when 0
+#   "" 
+#   when 1
+#     array.first
+#   when 2
+#     array.join(" and ")
+#   else
+#     [array[0..-2].join(", "), array.last].join(", and ")
+#   end
+# end
