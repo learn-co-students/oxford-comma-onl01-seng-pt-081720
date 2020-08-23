6e0182086614ccd9be2f710bce8fc37959670bca
Jazmin Matos <jmatos1993@gmail.com>
2020-08-23 00:27:56 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    new_array = array.pop
+    array.join(", ") + ", and " + new_array
+  end
 end
+
+#1 convert an array into a string
+#2 string = array.join(", ")
+# string << "and"
