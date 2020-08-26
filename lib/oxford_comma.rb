3e363dca64730ea72de8cc9fbededa9ca9fc7159
Corey Martin <coreyjmartin2@gmail.com>
2020-08-26 02:55:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
-def oxford_comma(array)
+#write a method #oxford_comma
+#oxford_comma returns a string without any additional formatting when given a 1-element array
+#.join turns an array into a string
+#put a comma in between all elements of an array EXCEPT
+#last element of the array should have an #and in front 
+#array[-1] calls on last element of an array
 
+def oxford_comma(array)
+ if array.size == 1
+  array.join (" ")
+ elsif array.size == 2 
+  array.join (" and ")
+ else array.size >= 3
+  array[-1] = "and " + array[-1]
+  array.join(", ")
+ end
 end
