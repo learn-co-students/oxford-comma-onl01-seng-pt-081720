349f1d2130575aa4f6b23db14dcba39c037719ae
Janae Quinones <Janaemassa@gmail.com>
2020-08-25 22:57:09 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+    array.join
+end
 
+def oxford_comma(array)
+    array.join(" and ")
+end
+
+def oxford_comma(array)
+    if array.size == 1
+        array.join
+    elsif array.size == 2
+        array.join(" and ")
+    else array.size >= 3
+        string_array = array.pop
+        array.join(", ") << string_array.unshift(", and ")
+    end
 end
