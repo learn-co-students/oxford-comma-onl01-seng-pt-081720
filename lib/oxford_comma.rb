9415e5fb6064636346cd298dd02206fc2791f44d
Jeff Michelot <jeffmichelot@gmail.com>
2020-08-26 14:29:30 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+    
+    if array.length == 2
+       return array.join (" and ") 
+    elsif  array.length > 2
+         array[-1] = "and " + array[-1]
+          
+       
+    end 
+    array.join(", ")
 end
