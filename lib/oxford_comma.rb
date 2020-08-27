a8138ee3d00d426de0ef7ed1239cb18470c0144c
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-08-27 07:31:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+if array.length == 1 
+  array.join
+  elsif array.length == 2
+     array.join(" and ")
+ else
+   last_index = array.last
+   array.pop
+    array<< "and #{last_index}"
+     array.join (", ")
+  
+ end
 end
