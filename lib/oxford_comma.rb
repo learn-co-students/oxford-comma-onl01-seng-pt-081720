bb94614f1513a72367ec6604858f718d475628e1
William T Cao <williamacao@gmail.com>
2020-08-28 18:35:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,8 @@ return "#{array[0]}"
 elsif array.length == 2
 return array.join(" and ")
 elsif array.length >= 3
+array[-1].insert(0, 'and ')
 return array.join(", ")
-array[-1].insert(-2, 'and')
 
 end
 end
