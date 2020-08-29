fdf64f9f5525ab3768d801482efad4762181d8bf
Haley Grassi <teacherhaleymarie@gmail.com>
2020-08-29 21:23:24 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
 
+  if array.length == 1
+return array[0]
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    return "#{array[0..-2].join(", ")}, and #{array.last}"
+     # add comma plus "and" for 3 element array
+  end
 end
+
+#length greater than 3
