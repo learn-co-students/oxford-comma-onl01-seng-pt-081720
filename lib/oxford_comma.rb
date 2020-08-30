636293396804d4376fdebd4261d8fc9ffec62b77
Myles Janik <mylesjanik@gmail.com>
2020-08-30 22:13:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,16 @@
 def oxford_comma(array)
+  
+  if array.length == 2
+    new_array = array.join(" and ")
+  end
+  
+  if array.length == 3
+    temp_holder = ""
+    array.pop
+    new_array = array.join()
+  
   new_array = array.join(", ")
+  
+  
   return new_array
 end
