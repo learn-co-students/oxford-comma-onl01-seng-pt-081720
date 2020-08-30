c73500e91460bbe23a75f17a2c07459231ffb6c7
Myles Janik <mylesjanik@gmail.com>
2020-08-30 22:22:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,21 +2,18 @@ def oxford_comma(array)
   
   if array.length == 2
     new_array = array.join(" and ")
-  end
   
   elsif array.length == 3
     temp_holder = array[2]
     array.pop
     new_array = array.join(", ")
     new_array.push(", and #{temp_holder}")
-  end
   
   elsif array.length > 3
     temp_holder = array[-1]
     array.pop
     new_array = array.join(", ")
     new_array.push(", and #{temp_holder}")
-  end
   
   else
     new_array = array.join(", ")
